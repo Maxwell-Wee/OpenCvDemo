4c1c64400e49d0321315da91f55c8d8f1b330842
王未未 <409535539@qq.com>
2019-03-06 14:30:18 +0800

    add opencv sample

--- app/src/main/java/com/subject/opencvdemo/MainActivity.java
+++ app/src/main/java/com/subject/opencvdemo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.subject.opencvdemo;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -19,9 +21,15 @@ import com.subject.opencvdemo.views.DrawView;
 
 import org.opencv.android.OpenCVLoader;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
 
@@ -68,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressLint("CheckResult")
     private void init() {
         CameraPreview cameraPreview = new CameraPreview(this);
         FrameLayout layout = findViewById(R.id.root_view);
@@ -84,26 +93,47 @@ public class MainActivity extends AppCompatActivity {
         cameraPreview.setOnClickListener(v -> cameraPreview.focus());
         DrawView drawView = findViewById(R.id.draw_layout);
         subject.concatMap(
-                cameraData -> OpenCVHelper.getRgbMat(new MatData(), cameraData.data, cameraData.camera))
-                .concatMap(matData -> OpenCVHelper.resize(matData, SIZE, SIZE))
-                .map(matData -> {
-                    matData.resizeRatio = (float) matData.oriMat.height() / matData.resizeMat.height();
-                    matData.cameraRatio = (float) cameraPreview.getHeight() / matData.oriMat.height();
-                    return matData;
+                new Function<CameraData, ObservableSource<? extends MatData>>() {
+                    @Override
+                    public ObservableSource<? extends MatData> apply(CameraData cameraData) throws Exception {
+
+
+                        Log.e("----->>>","ObservableSource");
+
+
+                        return OpenCVHelper.getRgbMat(new MatData(), cameraData.data, cameraData.camera);
+                    }
+                })
+                .concatMap(new Function<MatData, ObservableSource<? extends MatData>>() {
+                    @Override
+                    public ObservableSource<? extends MatData> apply(MatData matData) throws Exception {
+                        return OpenCVHelper.resize(matData, SIZE, SIZE);
+                    }
+                })
+                .map(new Function<MatData, MatData>() {
+                    @Override
+                    public MatData apply(MatData matData) throws Exception {
+                        matData.resizeRatio = (float) matData.oriMat.height() / matData.resizeMat.height();
+                        matData.cameraRatio = (float) cameraPreview.getHeight() / matData.oriMat.height();
+                        return matData;
+                    }
                 })
                 .concatMap(this::detectRect)
                 .compose(mainAsync())
-                .subscribe(matData -> {
-                    if (drawView != null) {
-                        if (matData.cameraPath != null) {
-                            drawView.setPath(matData.cameraPath);
+                .subscribe(new Consumer<MatData>() {
+                    @Override
+                    public void accept(MatData matData) throws Exception {
+                        if (drawView != null) {
+                            if (matData.cameraPath != null) {
+                                drawView.setPath(matData.cameraPath);
 
 
-                            Log.e("-->>path",matData.cameraPath.toString() );
-                        } else {
-                            drawView.setPath(null);
+                                Log.e("-->>path", matData.cameraPath.toString());
+                            } else {
+                                drawView.setPath(null);
+                            }
+                            drawView.invalidate();
                         }
-                        drawView.invalidate();
                     }
                 });
     }
