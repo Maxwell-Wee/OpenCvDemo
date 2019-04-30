4aa8bd87765647b81b8aeb56e7e59588d560c96e
王未未 <409535539@qq.com>
2019-04-30 16:03:30 +0800

    update file import

--- app/src/main/java/com/subject/opencvdemo/OpenVcActivity.java
+++ app/src/main/java/com/subject/opencvdemo/OpenVcActivity.java
@@ -1,25 +1,8 @@
 package com.subject.opencvdemo;
 
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-
-import org.opencv.android.Utils;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.CLAHE;
-import org.opencv.imgproc.Imgproc;
-
-
-
-import java.nio.ByteBuffer;
 
 public class OpenVcActivity extends AppCompatActivity {
 
