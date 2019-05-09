715ad68772fb9c8e2ba21436c96946f45a34ad00
王未未 <409535539@qq.com>
2019-05-09 16:21:25 +0800

    add alert

--- app/src/main/java/com/subject/opencvdemo/Test2Activity.java
+++ app/src/main/java/com/subject/opencvdemo/Test2Activity.java
@@ -32,6 +32,14 @@ public class Test2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setFloatingButton();
+
+
+        /*这个注释不要移除，在打开界面提示的时候，后期还有用*/
+//        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+//        intent.setData(Uri.parse("package:" + getPackageName()));
+//        startActivityForResult(intent, 100);
+
+
     }
 
     @Override
